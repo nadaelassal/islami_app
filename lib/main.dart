0f6964fc04cb6c0bd03eb7607799c9b69ea9a688
SERVER\portal2 <nadaelassal>
2024-08-13 12:02:26 +0200

    hadeth

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:islami_app/app_theme.dart';
 import 'package:islami_app/home_screen.dart';
+import 'package:islami_app/tabs/hadeth/hadeth_details_screen.dart';
 import 'package:islami_app/tabs/quran/sura_details_screen.dart';
 
 void main() {
@@ -19,6 +20,7 @@ class IslamiApp extends StatelessWidget {
       routes: {
         HomeScreen.routeName: (_) => HomeScreen(),
         SuraDetailsScreen.routeName: (_) => SuraDetailsScreen(),
+        HadethDetailsScreen.routeName: (_) => HadethDetailsScreen(),
       },
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
--- lib/tabs/hadeth/hadeth_tab.dart
+++ lib/tabs/hadeth/hadeth_tab.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami_app/tabs/hadeth/hadeth_details_screen.dart';
 
-class HadethTab extends StatelessWidget {
+import 'hadeth.dart';
+
+class HadethTab extends StatefulWidget {
+  @override
+  State<HadethTab> createState() => _HadethTabState();
+}
+
+class _HadethTabState extends State<HadethTab> {
+  List<Hadeth> ahadeth = [];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.brown,
+    return Column(
+      children: [
+        Image.asset(
+          'assets/images/hadeth_logo.png',
+          height: MediaQuery.of(context).size.height * 0.25,
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        Expanded(
+          child: ListView.separated(
+            itemBuilder: (_, index) => GestureDetector(
+              onTap: () => Navigator.of(context).pushNamed(
+                HadethDetailsScreen.routeName,
+              ),
+              child: Text(
+                ahadeth[index].title,
+                style: Theme.of(context).textTheme.headlineSmall,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            itemCount: ahadeth.length,
+            separatorBuilder: (_, __) => SizedBox(
+              height: 12,
+            ),
+          ),
+        ),
+      ],
     );
   }
+}
+
+loadAhadethFile() async {
+  String ahadethFileContent =
+      await rootBundle.loadString('assets/files/ahadeth.text');
+  List<String> ahadethText = ahadethFileContent.split('#');
 }
