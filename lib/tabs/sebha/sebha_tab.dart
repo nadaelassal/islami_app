8883e3308051376c340bc6519001193f39e3bba7
SERVER\portal2 <nadaelassal>
2024-08-27 10:16:12 +0200

    sebha

--- lib/tabs/sebha/sebha_tab.dart
+++ lib/tabs/sebha/sebha_tab.dart
@@ -1,6 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:islami_app/app_theme.dart';
+import 'package:islami_app/tabs/settings/settings_provider.dart';
+import 'package:provider/provider.dart';
 
 class SebhaTab extends StatefulWidget {
   @override
@@ -9,81 +15,99 @@ class SebhaTab extends StatefulWidget {
 
 class _SebhaTabState extends State<SebhaTab> {
   int counter = 0;
-  List<String> doaa = ['سبحان الله', 'الحمد لله', 'الله اكبر'];
-  double angle = 0;
   int index = 0;
+  double angle = 0;
+  List<String> doaa = ['الله اكبر', 'سبحان الله', 'الحمد لله'];
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Stack(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(
-                left: 30,
-              ),
-              child: Align(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  'assets/images/head_sebha_logo.png',
-                  height: 100,
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: counterMethod,
-              child: Padding(
-                padding: EdgeInsets.only(
-                  top: 75.0,
-                ),
+    SettingsProvider settingsProvider = Provider.of<SettingsProvider>(context);
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 30.0),
                 child: Align(
                   alignment: Alignment.center,
-                 child : Transform.rotate(angle:angle ,child:  Image.asset(
-                    'assets/images/body_sebha_logo.png',
+                  child: Image.asset(settingsProvider.isDark
+                      ? 'assets/images/head_sebha_dark.png'
+                      : 'assets/images/head_sebha_logo.png'),
+                ),
+              ),
+              GestureDetector(
+                onTap: counterMethod,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 75.0),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Transform.rotate(
+                      angle: angle,
+                      child: Image.asset(settingsProvider.isDark
+                          ? 'assets/images/body_sebha_dark.png'
+                          : 'assets/images/body_sebha_logo.png'),
+                    ),
                   ),
                 ),
               ),
-            ),),
-          ],
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        const Text(
-          "counter",
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 10),
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 13),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: Color.fromRGBO(183, 147, 95, 1).withOpacity(.57),
+            ],
           ),
-          child: Text(
-            counter.toString(),
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
+          const SizedBox(
+            height: 30,
           ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 10),
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 13),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: Color.fromRGBO(183, 147, 95, 1),
+          Text(
+            'Counter',
+            style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+                color:
+                    settingsProvider.isDark ? AppTheme.white : AppTheme.black),
           ),
-          child: Text(
-            doaa[index],
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: settingsProvider.isDark
+                  ? AppTheme.gold
+                  : Color.fromRGBO(183, 147, 95, 1).withOpacity(.57),
+            ),
+            child: Text(
+              counter.toString(),
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: settingsProvider.isDark
+                      ? AppTheme.white
+                      : AppTheme.black),
+            ),
           ),
-        ),
-      ],
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: settingsProvider.isDark
+                    ? AppTheme.gold
+                    : Theme.of(context).primaryColor),
+            child: Text(
+              doaa[index].toString(),
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w400,
+                  color: settingsProvider.isDark
+                      ? AppTheme.white
+                      : AppTheme.black),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   counterMethod() {
-    angle += 10 ;
+    angle += 10;
     if (counter == 33) {
       counter = 0;
       if (index < doaa.length - 1) {
@@ -94,8 +118,6 @@ class _SebhaTabState extends State<SebhaTab> {
     } else {
       counter++;
     }
-    setState(() {
-      
-    });
+    setState(() {});
   }
 }
